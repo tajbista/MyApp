6784491be6f2111797db06ae0a171b408fd406db
Taj Bist <tajbist2@gmail.com>
2019-08-13 20:29:52 -0400

    Made small change

--- src/main/java/com/HelloApp.java
+++ src/main/java/com/HelloApp.java
@@ -4,5 +4,7 @@ public class HelloApp {
 
     public static void main(String arg[]){
         System.out.println("Hello My App...!");
+
+        System.out.println("I want to learn Jenkins");
     }
 }
